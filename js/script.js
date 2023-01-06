ebc1df19f7675c851741008de82cf90f4e47f850
Your Name <wilkinsnigel2000@gmail.com>
2023-01-06 15:52:46 -0800

    done!

---
+++ js/script.js
@@ -0,0 +1,66 @@
+//check READ.ME
+let btnOne = document.getElementById("btnOne");
+let changeMeOne = document.getElementById("changeMeOne");
+
+let btnTwo = document.getElementById("btnTwo");
+let changeMeTwo = document.getElementById("changeMeTwo");
+
+let btnThree = document.getElementById("btnThree");
+let changeMeThree = document.getElementById("changeMeThree");
+
+let btnFour = document.getElementById("btnFour");
+let changeMeFour = document.getElementById("changeMeFour");
+
+let btnFive = document.getElementById("btnFive");
+let changeMeFive = document.getElementById("changeMeFive");
+
+let btnSix = document.getElementById("btnSix");
+let changeMeSix = document.getElementById("changeMeSix");
+
+let btnSeven = document.getElementById("btnSeven");
+let changeMeSeven = document.getElementById("changeMeSeven");
+
+let btnEight = document.getElementById("btnEight");
+let changeMeEight = document.getElementById("changeMeEight");
+
+let btnNine = document.getElementById("btnNine");
+let changeMeNine = document.getElementById("changeMeNine");
+
+let btnTen = document.getElementById("btnTen");
+let changeMeTen = document.getElementById("changeMeTen");
+
+btnOne.addEventListener("click", function(){
+    changeMeOne.innerText = "Look at me now!";
+});
+btnTwo.addEventListener("click", function(){
+    changeMeOne.innerText = "I'm getting paper!";
+});
+btnThree.addEventListener("click", function(){
+    changeMeOne.innerText = "Im fresher!";
+});
+btnFour.addEventListener("click", function(){
+    changeMeOne.innerText = "By Lil Wayne!";
+});
+btnFour.addEventListener("click", function(){
+    changeMeOne.innerText = "And chris breezy!";
+});
+btnFive.addEventListener("click", function(){
+    changeMeOne.innerText = "Busta Rhymes too!";
+});
+btnSix.addEventListener("click", function(){
+    changeMeOne.innerText = "Yellow Lamborghini!";
+});
+btnSeven.addEventListener("click", function(){
+    changeMeOne.innerText = "Yellow top missing!";
+});
+btnEight.addEventListener("click", function(){
+    changeMeOne.innerText = "Look like a toupee!";
+});
+btnNine.addEventListener("click", function(){
+    changeMeOne.innerText = "I get what you get in 10 years!";
+});
+btnTen.addEventListener("click", function(){
+    changeMeOne.innerText = "In two days!";
+});
+
+
